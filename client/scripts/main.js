9e551c4a14be37d57fadb500ab6bfd7224fbd972
sunhannie <hhy1770547592@163.com>
2018-03-29 19:00:22 +0800

    update main

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -75,6 +75,23 @@ var EventObject = {
     }
 };
 
+const setCookieVal = () => {
+    // Mark:check referrer
+    let referer = document.referrer;
+    if (referer!==''){
+        document.cookie = 'R = ' + referer ;
+        // SetCookie('R', referer , false , null , null)
+    }
+    // Mark:check ccode
+    var para = location.search.substring(1);
+    var pattern = /ccode/g;
+    if(pattern.test(para)){
+        document.cookie = 'ccode = ' + paraArr[0] ;
+        // SetCookie('ccode', paraArr[0] , false , null , null)
+    }
+};
+setCookieVal();
+
 var toPay = '';
 var paymentShadow = '';
 // var aLi = document.querySelectorAll('.openSub');
@@ -86,7 +103,7 @@ var openPayment = function(event){
     price = childNodes[2].value;
     var parentsNode = this.parentNode.parentNode.children;
     memberType = parentsNode[0].innerHTML;
-    var previewHTML = '<div id="payment-shadow" class="o-overlay-shadow fadeIn"></div><div id="payment-box" class="rightanswer show o-overlay__arrow-top fadeInRight"><div class="payment-title">欢迎订阅FT会员服务</div><div class="payment-way"><div class="payment-name"><span>会员类型：</span><span class="payment-type"><strong>'+memberType+'</strong></span></div><div class="payment-method"><span>支付方式：</span><label class="mode"><input name="pay" type="radio" value="ali" /><span id="pay-ali"></span> </label><label class="wxpay-mode"><input name="pay" type="radio" value="wxpay" /><span id="pay-wxpay"></span></label></div></div><div class="pay-action"><label>支付金额 '+price+'</label><button  class="to-pay" id="to-pay">确定支付</button></div></div>';
+    var previewHTML = '<div id="payment-shadow" class="o-overlay-shadow fadeIn"></div><div id="payment-box" class="rightanswer show o-overlay__arrow-top fadeInRight"><div class="payment-title">欢迎订阅FT会员服务</div><div class="payment-way"><div class="payment-name"><span>会员类型：</span><span class="payment-type"><strong>'+memberType+'</strong></span></div><div class="payment-method"><span>支付方式：</span><label class="mode"><input name="pay" type="radio" value="ali" checked /><span id="pay-ali"></span> </label><label class="wxpay-mode" style="display:none;"><input name="pay" type="radio" value="wxpay" /><span id="pay-wxpay"></span></label></div></div><div class="pay-action"><label>支付金额 '+price+'</label><button  class="to-pay" id="to-pay">确定支付</button></div></div>';
     paymentPage.innerHTML = previewHTML;
 
     toPay = document.getElementById('to-pay');
@@ -108,19 +125,19 @@ var pays = document.getElementsByName('pay');
 
 var toPayAction = function(event){
     for(let j = 0; j < pays.length; j++){
-        if(pays[j].checked === true){
+        if(pays[j].checked){
             payWay = pays[j].value;
         }
     }
  
     //满足2个条件：1.支付方式  2.会员类型
     if (memberType==='高端会员' && payWay==='ali') {
-        window.open('http://premium.ftacademy.cn/index.php/pay?offerId=8d5e7e72f12067991186cdf3cb7d5d9d');
-    }else if (memberType==='普通会员' && payWay==='ali') {
-        window.open('http://premium.ftacademy.cn/index.php/pay?offerId=eb6d8ae6f20283755b339c0dc273988b');
+        window.open('http://www.ftacademy.cn/index.php/pay?offerId=8d5e7e72f12067991186cdf3cb7d5d9d','_self');
+    }else if (memberType==='标准会员' && payWay==='ali','_self') {
+        window.open('http://www.ftacademy.cn/index.php/pay?offerId=eb6d8ae6f20283755b339c0dc273988b');
     }else if (memberType==='高端会员' && payWay==='wxpay') {
         window.open('#');
-    }else if (memberType==='普通会员' && payWay==='wxpay') {
+    }else if (memberType==='标准会员' && payWay==='wxpay') {
         window.open('#');
     }else{
 
@@ -135,24 +152,62 @@ var toPayAction = function(event){
 var closePayment = function(event){
     paymentPage.innerHTML = '';
 };
-// standard=0（非会员，默认premium=0）
-// standard=1（已经是标准会员，默认premium=0）
-// premium=1（已经是高端会员，默认standard=1）
-function updateUI(){
+let dataObj = {};
+
+const postUE = () => {
+    let cookieVal = {
+        uCookieVal : GetCookie('U'),
+        eCookieVal : GetCookie('E')
+    }
+    
+    let xhrpw = new XMLHttpRequest();
+    xhrpw.open('post','/index.php/jsapi/paywall');
+    xhrpw.onreadystatechange = function() {
+        if (xhrpw.readyState==4 && xhrpw.status==200){
+            var data = xhrpw.responseText;
+            dataObj = JSON.parse(data);
+            updateUI(dataObj);
+        } else {
+            // console.log('fail to post');
+        }
+    };
+    xhrpw.send(JSON.stringify(cookieVal));
+}
+
+ if (window.location.hostname === 'localhost' || window.location.hostname.indexOf('192.168') === 0 || window.location.hostname.indexOf('10.113') === 0 || window.location.hostname.indexOf('127.0') === 0) {
+        var paraArr = parseUrlSearch();//(2) ["premium=0", "standard=1"]
+        for(let j=0;j<paraArr.length;j++){
+            // paraArr[j].split('=');
+        }
+        var dataObj1=JSON.stringify(paraArr); //["premium=0","standard=1"]
+        dataObj = JSON.parse(dataObj1); //(2) ["premium=0", "standard=1"]
+        console.log(paraArr);  
+        updateUI(dataObj);
+}else{
+    postUE();
+}
+
+
+function updateUI(dataObj){
+    
     var premiumBtn = document.getElementById('premium-btn');
     var standardBtn = document.getElementById('standard-btn');
     var paraArr = parseUrlSearch();
+    
     if (isWeiXin()){
         EventObject.addHandler(standardBtn,"click",function(){return false;});
         EventObject.addHandler(premiumBtn,"click",function(){return false;});
     }else{
-        if (paraArr.includes('premium=0')&&paraArr.includes('standard=1')){
-            console.log(paraArr);
+        if ((dataObj.standard === 1 && dataObj.premium === 0)){
+        // if (paraArr.includes('premium=0')&&paraArr.includes('standard=1')){
+
             standardBtn.innerText = '已订阅';
             premiumBtn.innerText = '现在升级';
             EventObject.addHandler(standardBtn,"click",function(){return false;});
             EventObject.addHandler(premiumBtn,"click",openPayment);
-        }else if (paraArr.includes('standard=1')&&paraArr.includes('premium=1')){
+           
+        }else if (dataObj.standard === 1 && dataObj.premium === 1){
+        // }else if (paraArr.includes('standard=1')&&paraArr.includes('premium=1')){
             standardBtn.innerText = '已订阅';
             premiumBtn.innerText = '已订阅';
             EventObject.addHandler(standardBtn,"click",function(){return false;});
@@ -163,10 +218,11 @@ function updateUI(){
             EventObject.addHandler(standardBtn,"click",openPayment);
             EventObject.addHandler(premiumBtn,"click",openPayment);
         }
+        
     }
 
 }
-updateUI();
+
 
 function parseUrlSearch(){
     var para = location.search.substring(1);
@@ -178,38 +234,102 @@ function parseUrlSearch(){
 
 
 
-const postUE = () => {
-    let cookieVal = {
-        uCookieVal : GetCookie('U'),
-        eCookieVal : GetCookie('E')
-    }
-    let xhrpw = new XMLHttpRequest();
-    xhrpw.open('post','/index.php/jsapi/paywall');
-    xhrpw.onreadystatechange = function() {
-        if (xhrpw.readyState==4 && xhrpw.status==200){
-            var data = xhrpw.responseText;
-            // var dataObj = JSON.parse(data); 
-        } else {
-            console.log('fail to post');
-        }
-    };
-    xhrpw.send(cookieVal);
-}
 
-const setCookieVal = () => {
-    // Mark:check referrer
-    let referer = document.referrer;
-    if (referer!==''){
-        document.cookie = 'R = ' + referer ;
-        // SetCookie('R', referer , false , null , null)
-    }
-    // Mark:check ccode
-    var para = location.search.substring(1);
-    var pattern = /ccode/g;
-    if(pattern.test(para)){
-        document.cookie = 'ccode = ' + paraArr[0] ;
-        // SetCookie('ccode', paraArr[0] , false , null , null)
-    }
-};
-setCookieVal();
-postUE();
+
+
+/**
+ * 问题区域
+ */
+
+// var expanders = document.querySelectorAll('.o-expander');
+// for (let i = 0; i < expanders.length; i++) { 
+//     let ariaExpanded = expanders[i].getAttribute('aria-expanded');
+//     let ariaHidden = expanders[i].getAttribute('aria-hidden');
+//     var firstChild = expanders[i].children[0];
+    
+//     EventObject.addHandler(firstChild,"click", expandToggle(ariaExpanded,ariaHidden));
+
+    
+// }
+
+// function expandToggle(ariaExpanded,ariaHidden){ 
+    
+    // let bbb = this.children[0].children[0];
+            // let bb = this.children[0].children[0];
+        // console.log(bb); 
+        // if (ariaExpanded==='false'){
+        //     this.parentNode.setAttribute('aria-expanded','true'); 
+        //     bb.style.animation = "arrowRotateDown 0.25s 1 forwards ease-in";
+        // }else{
+        //     this.parentNode.setAttribute('aria-expanded','false');
+        //     bb.style.animation = "arrowRotateUp 0.25s forwards ease-out";
+        // }
+
+        // let nextSbl = this.nextSibling;
+        // if (ariaHidden==='false'){
+        //     console.log('ff'+aa);
+        //     nextSbl.setAttribute('aria-hidden','true'); 
+        //     nextSbl.style.maxHeight = '615px';
+        //     nextSbl.style.transition = "max-height 0.25s ease";
+        // }else{
+        //     nextSbl.setAttribute('aria-hidden','false');
+        //     nextSbl.style.maxHeight = '0px';
+        //     nextSbl.style.transition = "max-height 0.25s ease";
+
+        // }
+
+// }
+
+// var toggleTags = document.querySelectorAll('.toggle');
+// var expanderContents = document.querySelectorAll('.o-expander__content');
+// for (let i = 0; i < toggleTags.length; i++) {   
+//     EventObject.addHandler(toggleTags[i],"click",expanderToggle);
+// }
+// function expanderToggle(){  
+//     let aa = this.children[0].getAttribute('aria-expanded');
+//     console.log('expanderToggle'+aa);
+//     let bb = this.children[0].children[0].children[0];
+//     // console.log('bb:'+bb.innerHTML);
+//     if (aa==='false'){
+//         console.log('ff'+aa);
+//         this.children[0].setAttribute('aria-expanded','true'); 
+//         bb.style.animation = "arrowRotateDown 0.25s 1 forwards ease-in";
+//     }else{
+//         this.children[0].setAttribute('aria-expanded','false');
+//         bb.style.animation = "arrowRotateUp 0.25s forwards ease-out";
+//     }
+// }
+
+
+// for (let i = 0; i < expanderContents.length; i++) {   
+//     EventObject.addHandler(expanderContents[i],"click",expanderContent);
+// }
+// function expanderContent(){  
+//     let aa = this.getAttribute('aria-hidden');
+//     console.log('expanderContent:--'+aa);
+//     if (aa==='false'){
+//         console.log('ff'+aa);
+//         this.setAttribute('aria-hidden','true'); 
+//         this.style.maxHeight = '615px';
+//         this.style.transition = "max-height 0.25s ease";
+//     }else{
+//         this.setAttribute('aria-hidden','false');
+//         this.style.maxHeight = '0px';
+//         this.style.transition = "max-height 0.25s ease";
+
+//     }
+// }
+
+
+
+//     max-height: 615px;
+//     -webkit-transition: max-height 0.25s ease,border-top 0.10s ease;
+//     transition: max-height 0.25s ease,border-top 0.10s 
+//    border-top: 1px solid #cec6b9;
+
+//    transition: property duration timing-function delay;
+
+//      max-height: 0;
+//     -webkit-transition: max-height 0.25s ease,border-top 0.75s ease;
+//     transition: max-height 0.25s ease,border-top 0.75s ease;
+//     border-top: 1px solid transparent;
