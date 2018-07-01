228558fce499b7564ec005e988c7111d63f3855f
sunhannie <hhy1770547592@163.com>
2018-07-01 14:33:20 +0800

    update redme

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 ## 启动步骤
 npm install
+
 gulp serve
 
 ## 技术栈
 ES6 + scss + nunjucks + node + gulp + rollup
 
 ## gulp 任务
-gulp build-page 编译出html页面
-gulp build  编译出文件
-gulp serve  启动本地服务器
-gulp copy   clean, build, copyHtml  发布
-gulp copyTest  仅仅发布subscriptionTest.html文件到ftac项目中
+- gulp build-page 编译出html页面
+- gulp build  编译出文件
+- gulp serve  启动本地服务器
+- gulp copy   clean, build, copyHtml  发布
+- gulp copyTest  仅仅发布subscriptionTest.html文件到ftac项目中
 
 ## 文件定位
 views/data/data.json ：保存页面显示文字
--- client/scripts/exchange.js
+++ client/scripts/exchange.js
@@ -1,14 +1,48 @@
-import {EventObject,getUrlParams,isEmptyObj} from './subscribe_api.js';
+import {EventObject,GetCookie,getUrlParams,isEmptyObj} from './subscribe_api.js';
 
 
 let tokenVal = getUrlParams('token');
 let tokenId = document.getElementById('token');
 tokenId.value = tokenVal;
-// console.log(tokenId.value);
 
-// let cancel = document.querySelector('.cancel');
+let conform = document.querySelector('.conform');
 
-// EventObject.addHandler(cancel,"click", function(){
-//     let exchange = document.querySelector('[name="exchange"]');
-//     exchange.value = '';
-// });  
+EventObject.addHandler(conform,"click", function(){
+    let userId = GetCookie('USER_ID') || '';
+    let exchange = document.querySelector('[name="exchange"]');
+    let exchangeVal = exchange.value;
+    // console.log('exchangeVal'+exchangeVal);
+    if(exchangeVal === ''){
+        alert('请输入信息！');
+    }else if(exchangeVal.length < 16){
+        alert('请正确输入兑换码！！！');
+    }else{
+        if (!!userId){
+            var xhrpw = new XMLHttpRequest();
+            xhrpw.open('post', '/users/setting/binding');
+            xhrpw.setRequestHeader('Content-Type', 'application/text');
+            var exchangeInfo = {
+                exchange:exchangeVal,
+                token:tokenVal
+            }
+            xhrpw.onload = function() {
+                if (xhrpw.status === 200) {
+                    var data = xhrpw.responseText;
+                    // var dataObj = JSON.parse(data);
+                    console.log(data)
+                    // if(!dataObj.errcode){
+                    //     window.open('/','_self');
+                    // }else{
+                    //     alert('请正确输入兑换码！');
+                    // }
+                } else {
+                    alert('请正确输入兑换码！！');
+                }
+            };     
+            xhrpw.send(JSON.stringify(exchangeInfo));
+        }
+    }
+    
+
+});  
+
